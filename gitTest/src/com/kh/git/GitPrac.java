55177cb02cf5ce69881f0be315b0e45382621dc0
user <user@daseulkim>
2020-04-24 11:11:40 +0900

    세번째커밋

--- gitTest/src/com/kh/git/GitPrac.java
+++ gitTest/src/com/kh/git/GitPrac.java
@@ -3,6 +3,8 @@ package com.kh.git;
 public class GitPrac {
 	public static void main(String[] args) {
 		System.out.println("hello");
+		System.out.println("world");
+		System.out.println("thxxxxxxx");
 	}
 
 }
